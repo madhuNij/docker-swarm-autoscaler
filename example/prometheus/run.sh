2da59c16f651e22f54e7727a95e7bb06799ff125
root <root@node0.g10-project.advosuwmadison-pg0.wisc.cloudlab.us>
2023-05-06 22:59:26 -0500

    Adding the new compose files and new grafana folders

--- example/prometheus/run.sh
+++ example/prometheus/run.sh
@@ -1,3 +1,1 @@
-docker stack rm g10
-sleep 10 # Wait for stack remove to complete
 docker stack deploy g10 -c docker-compose.yml
--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 docker ps -aq | xargs docker stop | xargs docker rm
 docker build -t docker-swarm-autoscaler .
-cd example/prometheus
+cd example/prometheus 
+docker stack rm g10
+sleep 10 # Wait for stack remove to complete
